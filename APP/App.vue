1f6f8bbda2d4db94d11091490173d8abb5b0776f
chlengkey <chrisdityralengkey@gmail.com>
2021-05-06 17:50:46 +0800

    Web

---
+++ APP/App.vue
@@ -0,0 +1,64 @@
+<template>
+
+  <h1>{{ title }}</h1>
+  <p>Selesai Menambahkan Data?</p>
+
+  <div v-if="showModal">
+
+    <Modal theme="sale" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">more info</a>
+        <a href="#">status blockchain</a>
+      </template>
+
+      <h1>Berhasil Ditambahkan</h1>
+      <p>Data yang telah ditambahkan tidak dapat diubah kembali</p>
+    </Modal>
+
+  </div>
+
+  <button @click="toggleModal">Tambahkan Data</button>
+  
+</template>
+
+<script>
+import { Modal } from './views/Modal.vue'
+
+export default {
+  name: 'App',
+  views: { 
+      Modal
+  },
+  data() {
+    return {
+      title: 'Medical Chain',
+      showModal: false,
+      showModalTwo: false
+    }
+  },
+  methods: {
+    toggleModal(){
+      this.showModal = !this.showModal
+    },
+    toggleModalTwo(){
+      this.showModalTwo = !this.showModalTwo
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
---
+++ APP/views/Login.vue
@@ -0,0 +1,69 @@
+<template>
+    <form @Masuk.prevent="handleMasuk">
+        <label>Email:</label>
+        <input type="email" required v-model="email">
+
+        <label>Password:</label>
+        <input type="password" required v-model="password">
+        <div v-if="passwordError" class="error">{{ passwordError }}</div>
+
+        <div class="Masuk">
+            <button>Masuk</button>
+        </div>
+    </form>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            email: '',
+            password: '',
+            passwordError: ''
+        }
+    },
+    methods: {
+        handleMasuk() {
+            this.passwordError = this.password.length > 5 ? '' : 'Password min.8 karakter'
+        }
+    }
+}
+</script>
+
+<style>
+    form {
+        max-width: 420px;
+        margin: 30px auto;
+        background: white;
+        text-align: left;
+        padding: 40px;
+        border-radius: 10px;
+    }
+    label{
+        color: #aaa;
+        display: inline-block;
+        margin: 25px 0 15px;
+        font-size: 0.6em;
+        text-transform: uppercase;
+        letter-spacing: 1px;
+        font-weight: bold;
+    }
+    input{
+        display: block;
+        padding: 10px 6px;
+        width: 100%;
+        box-sizing: border-box;
+        border: none;
+        border-bottom: 1px solid #ddd;
+        color: #555;
+    }
+    button {
+        background: orangered;
+        border: 0;
+        padding: 10px 20px;
+        margin-top: 20px;
+        color: white;
+        border-radius: 10px;
+    }
+</style>
