64fc2bb5f62e005254506012f18418c644859713
Cleonart <zredhard@gmail.com>
2021-05-14 19:44:10 +0800

    added and install axios to the app

--- APP/src/main.js
+++ APP/src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/index.css'
+import Axios from 'axios'
 
+Vue.prototype.$http = Axios;
 createApp(App).use(router).mount('#app')
