6c74044765b882de13b5eaa6030507c5f5bd7f29
Cleonart <zredhard@gmail.com>
2021-05-14 19:42:15 +0800

    added environment variable to the app

--- APP/src/App.vue
+++ APP/src/App.vue
@@ -2,57 +2,22 @@
 
   <div class="flex">
     <Sidebar/>
+    URL : {{BASE_URL}}
     <router-view/>
   </div>
-  <!--
-  <h1>{{ title }}</h1>
-  <p>Selesai Menambahkan Data?</p>
-
-  <div v-if="showModal">
-
-    <Modal theme="sale" @close="toggleModal">
-      <template v-slot:links>
-        <a href="#">more info</a>
-        <a href="#">status blockchain</a>
-      </template>
-
-      <h1>Berhasil Ditambahkan</h1>
-      <p>Data yang telah ditambahkan tidak dapat diubah kembali</p>
-    </Modal>
-
-  </div>
-
-  <button @click="toggleModal">Tambahkan Data</button>-->
-  
 </template>
 
 <script>
-//import { Modal } from '@/views/Modal.vue'
-import Sidebar from '@/modules/sidebar.vue'
-import Dashboard from '@/views/Dashboard.vue'
-
+import Sidebar from '@/modules/sidebar.vue';
 export default {
   name: 'App',
-  components : {Sidebar, Dashboard},
-  views: { 
-  //    Modal,
-      Dashboard
-  },
+  components : {Sidebar},
   data() {
     return {
       title: 'Medical Chain',
-      showModal: false,
-      showModalTwo: false
+      BASE_URL: process.env.VUE_APP_API
     }
   },
-  methods: {
-    toggleModal(){
-      this.showModal = !this.showModal
-    },
-    toggleModalTwo(){
-      this.showModalTwo = !this.showModalTwo
-    }
-  }
 }
 </script>
 
